f37bd54f9e8d799f1f494e7d5d37126b9c49c7b9
Victor <e2301757@vamk.fi>
2024-04-17 09:02:41 +0300

    Päätiedostot

---
+++ main.c
@@ -0,0 +1,71 @@
+/**
+ * file:    template.c
+ * author:  John Doe
+ * date:    2024-03-13
+ * version: 1.0
+ *
+ * brief: A brief description of what the program does.
+ *
+ * Detailed description of the program. This program is designed to 
+ * demonstrate modern C programming practices, including structured 
+ * documentation, use of constants, function prototypes, and 
+ * basic program structure.
+ *
+ * This software is released under the GPL License.
+ */
+
+/* Includes */
+#include <stdio.h>
+
+/* Definitions and Constants */
+#define COLUMNS 10
+
+/* Function Prototypes */
+
+struct cell { 
+    int current;
+    int future; 
+}; 
+
+/* Main Function */
+int main(void)
+{
+    struct cell board [9] [9] = {0, 0}; 
+}
+
+/* Functions */
+/**
+ * brief Example function description.
+ *
+ * More detailed explanation of what the function does, including any algorithms or
+ * computational logic used. This section can also describe the context in which the
+ * function should be used.
+ *
+ * input: param1 Description of the first parameter, including how it's used.
+ * input: param2 Description of the second parameter, including how it's used.
+ *
+ * return: Description of the return value, including possible values and their meanings.
+ */
+
+
+/**
+ * brief: License Information
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
