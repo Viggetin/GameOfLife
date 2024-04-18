8f21f9a1cee1cbc8d1dc3ab1410235043c74e97e
Victor <victor.sten@outlook.com>
2024-04-18 22:00:11 +0300

    "Kommentoitiin viellä funktioiden toiminnat ylös"

--- main.c
+++ main.c
@@ -55,7 +55,15 @@ int main(void)
 }
 
 /* Functions */
-
+/**
+ * Lyhyt selitys ohjelman toiminnasta
+ *
+ * Funktion idea on aloittaa itse peli. Ensimmäisenä nollaamalla 8x8 taulukon nolliksi molemmat current ja futuren
+ * Current taulukon idea on olla se joka printataan esille ja joka päivitetään aina pelin tapahtumien jälkeen
+ * Future toimii siis ns Temp muuttujana jonne tallennetaan pelin muutokset ennen kun korvataan current taulukko uudella.
+ * 
+ * Funktiossa asetetaan myös elävät solut jota voi muokata tarvittaessa
+ */
 void game_init(struct cell board[ROWS][COLUMNS])
 {
     //Nollataan board.Current
@@ -79,18 +87,28 @@ void game_init(struct cell board[ROWS][COLUMNS])
     board[4][4].current = 1;
 
 }
-
+/**
+ * Lyhyt selitys ohjelman toiminnasta
+ *
+ * Funktion idea on aloittaa pelin "toteuttamisen" annettujen sääntöjen mukaisesti
+ * funktio aloittaa käymällä läpi taulukon joka ikisen solun läpi josta tarkistetaan countNeighbors funktion avulla solun naapureiden lukumäärän
+ * Naapureiden lukumäärän perusteella voidaan ehtolauseilla päätellä tuleeko tämän hetkinen solu elämään tai menehtymään
+ * Funktion lopussa taulukko siiretään updateBoard funktioon
+ * 
+ */
 void calculateSim(struct cell board[ROWS][COLUMNS])
 {
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLUMNS; j++) {
         int count = countNeighbors(board, i, j);
         if (board[i][j].current == 1) // Cell is currently alive
-            {  
+            {
+                //Elävällä solulla on vain yksi naapuri tai 4 naapuria josta solu menehtyy joko yksinäisyyteen tai ylikansoittumiseen
                 if(count < 2 || count > 3)
                 {
                     board[i][j].future = 0;
                 }
+                // Muuten jos solulla on 3 naapuria niin se jatkaa elämistä
                 else
                 {
                     board[i][j].future = 1;
@@ -98,6 +116,7 @@ void calculateSim(struct cell board[ROWS][COLUMNS])
             }
                 else
             {
+                // Jos solu on tyhjä mutta sillä on kolme naapuria tulee solusta elävä
                 if(count == 3)
                 {
                     board[i][j].future = 1;
@@ -111,6 +130,15 @@ void calculateSim(struct cell board[ROWS][COLUMNS])
     }
     updateBoard(board);
 }
+/**
+ * Lyhyt selitys ohjelman toiminnasta
+ *
+ * Funktion idea on laskea nykyisen tarkkailun alla olevan solun ympärillä olevien naapureiden lukumäärän
+ * Funktiossa tämä tarkastellaan for looppeja käyttäen vasemmalta oikealle ja ylhäältä alas
+ * Funktio erikseen tarkistaa, että se ei ota huomioon itse tarkasteltavaa solua
+ * 
+ * Funktio palautta count muuttujan, jota käytetään hyväksi calculateSim funktiossa
+ */
 int countNeighbors(struct cell board[ROWS][COLUMNS], int r, int c)
 {
     int count = 0;
@@ -119,10 +147,10 @@ int countNeighbors(struct cell board[ROWS][COLUMNS], int r, int c)
     {
         for (int j = c - 1; j <= c + 1; j++) 
         {
-            // Continue if it's the cell itself
+            // Jatka jos kyseessä on tarkasteltava solu
             if (i == r && j == c)
                 continue;
-            // Count if the neighboring cell is 1
+            // plussataan naapuri jos on
             if (board[i][j].current == 1)
             {
                 count++;
@@ -131,7 +159,11 @@ int countNeighbors(struct cell board[ROWS][COLUMNS], int r, int c)
     }
     return count;
 }
-// Funktiossa käydään kaikki board arrayn solut läpi jotka printataan näkyville
+/**
+ * Lyhyt selitys ohjelman toiminnasta
+ *
+ * Funktiossa printataan nykyinen voimassa oleva taulu esille terminaaliin
+ */
 void printBoard(struct cell board[ROWS][COLUMNS]) {
     printf("Current state of the board:\n");
     for (int i = 0; i < ROWS; i++) {
@@ -142,7 +174,12 @@ void printBoard(struct cell board[ROWS][COLUMNS]) {
     }
     printf("\n");
 }
-
+/**
+ * Lyhyt selitys ohjelman toiminnasta
+ *
+ * Funktio korvaa vanhemman taulukon future taululla 
+ * joka on päivitetty alkuperäisestä calculateSim funktion laskujen mukaisesti
+ */
 void updateBoard(struct cell board[ROWS][COLUMNS])
 {
     for (int r = 0; r < ROWS; r++) {
